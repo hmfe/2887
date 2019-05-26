28d32048791ae0c34c2ea069ead34b2bb4a88936
Jimmy <jimhog2@outlook.com>
2019-05-26 22:46:41 +0200

    Add base layout and search functionality

---
+++ scripts.js
@@ -0,0 +1,122 @@
+const resetInnerHtml = element => (element.innerHTML = null);
+
+const generateUuid = () => {
+  let dt = new Date().getTime();
+  const uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, c => {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+};
+
+const formatDate = date => {
+  const appendZero = number => (number < 10 ? `0${number}` : number);
+
+  return `${date.getFullYear()}-${appendZero(date.getMonth() + 1)}-${appendZero(
+    date.getDate()
+  )} ${appendZero(date.getHours())}:${appendZero(date.getMinutes())}`;
+};
+
+const clearSearchHistory = () => {
+  localStorage.setItem("searchHistory", null);
+  updateSearchHistoryList();
+};
+
+const getSearchHistory = () => {
+  const searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  return searchHistory ? searchHistory : [];
+};
+
+const setSearchHistory = newHistory =>
+  localStorage.setItem("searchHistory", JSON.stringify(newHistory));
+
+const addToSearchHistory = newHistoryItem => {
+  setSearchHistory([...getSearchHistory(), newHistoryItem]);
+  updateSearchHistoryList();
+};
+
+const clearSearchInput = () =>
+  (document.getElementById("search-input").value = "");
+
+const updateSearchResult = (listElement, shows) => {
+  const handleClick = newHistoryItem => {
+    addToSearchHistory(newHistoryItem);
+    clearSearchInput();
+    resetInnerHtml(listElement);
+  };
+
+  shows.forEach(show => {
+    const li = document.createElement("li");
+
+    const newHistoryItem = {
+      id: generateUuid(),
+      show,
+      date: formatDate(new Date())
+    };
+
+    li.innerText = show;
+    li.onclick = () => handleClick(newHistoryItem);
+    listElement.appendChild(li);
+  });
+};
+
+const fetchShowsByQueryString = queryString => {
+  const el = document.getElementById("show-list");
+
+  resetInnerHtml(el);
+
+  const getShowsFromData = data => data.map(showData => showData.show.name);
+
+  fetch(`http://api.tvmaze.com/search/shows?q=${queryString}`).then(res =>
+    res.json().then(data => updateSearchResult(el, getShowsFromData(data)))
+  );
+};
+
+const updateSearchHistoryList = () => {
+  const el = document.getElementById("search-history-list");
+
+  resetInnerHtml(el);
+
+  const handleDelete = id => {
+    const newHistory = getSearchHistory().filter(show => show.id !== id);
+    setSearchHistory(newHistory);
+    updateSearchHistoryList();
+  };
+
+  const generateListItem = historyItem => {
+    const li = document.createElement("li");
+
+    const name = document.createElement("strong");
+    name.className = "search-history-item-name";
+    name.innerText = historyItem.show;
+
+    const dateAndDeleteContainer = document.createElement("div");
+    dateAndDeleteContainer.className = "date-and-delete";
+
+    const date = document.createElement("div");
+    date.className = "search-history-item-date";
+    date.innerText = historyItem.date;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "search-history-item-delete";
+    deleteBtn.innerText = "Delete";
+    deleteBtn.onclick = () => handleDelete(historyItem.id);
+
+    dateAndDeleteContainer.appendChild(date);
+    dateAndDeleteContainer.appendChild(deleteBtn);
+
+    li.appendChild(name);
+    li.appendChild(dateAndDeleteContainer);
+
+    return li;
+  };
+
+  getSearchHistory().forEach(historyItem =>
+    el.appendChild(generateListItem(historyItem))
+  );
+};
+
+window.onload = () => {
+  updateSearchHistoryList();
+};
