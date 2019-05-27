98670198b88433d73d05a9dc4f6d688442ab5edb
Jimmy <jimhog2@outlook.com>
2019-05-27 19:45:58 +0200

    Move event listeners to the JavaScript code

--- scripts.js
+++ scripts.js
@@ -10,13 +10,12 @@ const generateUuid = () => {
   return uuid;
 };
 
-const formatDate = date => {
-  const appendZero = number => (number < 10 ? `0${number}` : number);
+const appendZero = number => (number < 10 ? `0${number}` : number);
 
-  return `${date.getFullYear()}-${appendZero(date.getMonth() + 1)}-${appendZero(
+const formatDate = date =>
+  `${date.getFullYear()}-${appendZero(date.getMonth() + 1)}-${appendZero(
     date.getDate()
   )} ${appendZero(date.getHours())}:${appendZero(date.getMinutes())}`;
-};
 
 const clearSearchHistory = () => {
   localStorage.setItem("searchHistory", null);
@@ -39,44 +38,51 @@ const addToSearchHistory = newHistoryItem => {
 const clearSearchInput = () =>
   (document.getElementById("search-input").value = "");
 
-const updateSearchResult = (listElement, shows) => {
-  const handleMouseDown = event => event.preventDefault();
+const handleSearchResultItemMouseDown = event => event.preventDefault();
 
-  const handleClick = async newHistoryItem => {
-    addToSearchHistory(newHistoryItem);
-    // Awaiting the clearSearchInput() function to make sure the search-input value is empty
-    await clearSearchInput();
-    resetInnerHtml(listElement);
+const handleSearchResultItemClick = async newHistoryItem => {
+  addToSearchHistory(newHistoryItem);
+  // Awaiting the clearSearchInput() function to make sure the search-input value is empty
+  await clearSearchInput();
+  resetInnerHtml(document.getElementById("show-list"));
+};
+
+const generateSearchResultItem = show => {
+  const li = document.createElement("li");
+
+  const newHistoryItem = {
+    id: generateUuid(),
+    show,
+    date: formatDate(new Date())
   };
 
-  shows.forEach(show => {
-    const li = document.createElement("li");
+  li.innerText = show;
+  // Handling mousedown to stop the blur-event from the search-input
+  li.onmousedown = handleSearchResultItemMouseDown;
+  li.onclick = async () => {
+    await handleSearchResultItemClick(newHistoryItem);
+    setShowListVisibility();
+  };
 
-    const newHistoryItem = {
-      id: generateUuid(),
-      show,
-      date: formatDate(new Date())
-    };
-
-    li.innerText = show;
-    // Handling mousedown to stop the blur-event from the search-input
-    li.onmousedown = handleMouseDown;
-    li.onclick = () => handleClick(newHistoryItem).then(setShowListVisibility);
-    listElement.appendChild(li);
-  });
+  return li;
 };
+
+const updateSearchResult = (listElement, shows) =>
+  shows.forEach(show =>
+    listElement.appendChild(generateSearchResultItem(show))
+  );
 
 const handleInputChange = value => {
   fetchShowsByQueryString(value);
   setShowListVisibility();
 };
 
+const getShowsFromData = data => data.map(showData => showData.show.name);
+
 const fetchShowsByQueryString = queryString => {
   const el = document.getElementById("show-list");
 
   resetInnerHtml(el);
-
-  const getShowsFromData = data => data.map(showData => showData.show.name);
 
   fetch(`http://api.tvmaze.com/search/shows?q=${queryString}`).then(res =>
     res.json().then(data => updateSearchResult(el, getShowsFromData(data)))
@@ -92,49 +98,75 @@ const setShowListVisibility = () => {
   showList.style.display = input.value === "" ? "none" : "block";
 };
 
+const handleHistoryItemDelete = id => {
+  const newHistory = getSearchHistory().filter(show => show.id !== id);
+  setSearchHistory(newHistory);
+  updateSearchHistoryList();
+};
+
+const generateHistoryListItem = historyItem => {
+  const li = document.createElement("li");
+
+  const name = document.createElement("b");
+  name.className = "search-history-item-name";
+  name.innerText = historyItem.show;
+
+  const dateAndDeleteContainer = document.createElement("div");
+  dateAndDeleteContainer.className = "date-and-delete";
+
+  const date = document.createElement("div");
+  date.className = "search-history-item-date";
+  date.innerText = historyItem.date;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "fas fa-times search-history-item-delete";
+  deleteBtn.onclick = () => handleHistoryItemDelete(historyItem.id);
+
+  dateAndDeleteContainer.appendChild(date);
+  dateAndDeleteContainer.appendChild(deleteBtn);
+
+  li.appendChild(name);
+  li.appendChild(dateAndDeleteContainer);
+
+  return li;
+};
+
 const updateSearchHistoryList = () => {
   const el = document.getElementById("search-history-list");
 
   resetInnerHtml(el);
 
-  const handleDelete = id => {
-    const newHistory = getSearchHistory().filter(show => show.id !== id);
-    setSearchHistory(newHistory);
-    updateSearchHistoryList();
-  };
-
-  const generateListItem = historyItem => {
-    const li = document.createElement("li");
-
-    const name = document.createElement("b");
-    name.className = "search-history-item-name";
-    name.innerText = historyItem.show;
-
-    const dateAndDeleteContainer = document.createElement("div");
-    dateAndDeleteContainer.className = "date-and-delete";
-
-    const date = document.createElement("div");
-    date.className = "search-history-item-date";
-    date.innerText = historyItem.date;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "fas fa-times search-history-item-delete";
-    deleteBtn.onclick = () => handleDelete(historyItem.id);
-
-    dateAndDeleteContainer.appendChild(date);
-    dateAndDeleteContainer.appendChild(deleteBtn);
-
-    li.appendChild(name);
-    li.appendChild(dateAndDeleteContainer);
-
-    return li;
-  };
-
   getSearchHistory().forEach(historyItem =>
-    el.appendChild(generateListItem(historyItem))
+    el.appendChild(generateHistoryListItem(historyItem))
+  );
+};
+
+const addEventListeners = () => {
+  const clearHistoryButton = document.getElementById("clear-history-button");
+  clearHistoryButton.addEventListener("click", clearSearchHistory);
+
+  const searchInput = document.getElementById("search-input");
+  searchInput.addEventListener("blur", handleBlur);
+  searchInput.addEventListener("focus", setShowListVisibility);
+  searchInput.addEventListener("input", () =>
+    handleInputChange(searchInput.value)
+  );
+  window.addEventListener("unload", removeEventListeners);
+};
+
+const removeEventListeners = () => {
+  const clearHistoryButton = document.getElementById("clear-history-button");
+  clearHistoryButton.removeEventListener("click", clearSearchHistory);
+
+  const searchInput = document.getElementById("search-input");
+  searchInput.removeEventListener("blur", handleBlur);
+  searchInput.removeEventListener("focus", setShowListVisibility);
+  searchInput.removeEventListener("input", () =>
+    handleInputChange(searchInput.value)
   );
 };
 
 window.onload = () => {
   updateSearchHistoryList();
+  addEventListeners();
 };
